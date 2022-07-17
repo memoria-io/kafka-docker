beed29b0e2c7a6d615ed85ee6376c542518de99a
Ismail Marmoush <marmoushismail@gmail.com>
2022-07-17 18:14:08 +0200

    config transformation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kafka-docker
+
+## To build
+```bash
+docker build --build-arg KAFKA_VERSION=3.2.0 -t local/kafka:3.2.0 .
+```
+
+## To run
+
+```bash
+docker run -it --env-file default_env local/kafka:3.2.0 bash
+```
--- main.sh
+++ main.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
-UUID=A0acq9cPQwymi60FufxF7g
 SERVER_CONFIG_FILE=/kafka/config.properties
 PREFIX=KAFKA_CONFIG_
+set +x
+set -e
+
+nodeId=${hostname#*-}
+if [ -z "${nodeId}" ];
+then
+  export KAFKA_CONFIG_node_id="0"
+else
+  export KAFKA_CONFIG_node_id=$nodeId
+fi
+
+echo "Node id: ${KAFKA_CONFIG_node_id}"
 
 echo "######## Kafka Configuration #########" > $SERVER_CONFIG_FILE
-env | grep "^${PREFIX}" | awk -F $PREFIX '{print $2}' >> $SERVER_CONFIG_FILE
+env | grep "^${PREFIX}" | \
+ awk -F $PREFIX '{print $2}' | \
+ sed -e ':b; s/^\([^=]*\)*__/\1\##/; tb;' | \
+ sed -e ':b; s/^\([^=]*\)*_/\1\./; tb;' | \
+ sed -e ':b; s/^\([^=]*\)*##/\1\_/; tb;' >> $SERVER_CONFIG_FILE
 
-#./kafka/bin/kafka-storage.sh random-uuid
-./kafka/bin/kafka-storage.sh format -t ${UUID} -c ${SERVER_CONFIG_FILE}
+echo "---------------------------------"
+echo "Kafka Configuration at ${SERVER_CONFIG_FILE}"
+echo "---------------------------------"
+cat $SERVER_CONFIG_FILE
+echo "---------------------------------"
+
+export KAFKA_NODE_UUID=$(./kafka/bin/kafka-storage.sh random-uuid)
+./kafka/bin/kafka-storage.sh format -t ${KAFKA_NODE_UUID} -c ${SERVER_CONFIG_FILE}
 ./kafka/bin/kafka-server-start.sh ${SERVER_CONFIG_FILE}
