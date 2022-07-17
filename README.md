040b8ec21f70b9911ee454a0cd0766f81546f390
Ismail Marmoush <marmoushismail@gmail.com>
2022-07-17 22:14:07 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
 # kafka-docker
+This Kafka docker uses apache kafka
+The default configs runs in kraft mode (no zookeeper mode), yet it can be used with normal zk instances. 
 
-## For user
+## Quick usage
+
+* To run directly from docker-hub
+
 ```bash
 docker run -it --env-file default_env --hostname kafka-0 memoriaio/kafka-docker:3.2.0
 ```
 
+* To build and run locally
 
-##  For committer
 ```bash
 docker build --build-arg KAFKA_VERSION=3.2.0 -t local/kafka:3.2.0 .
 ```
 
-## To run with defaults, and node.id=0
+* To run with defaults, and node.id=0
 
 ```bash
 docker run -it --env-file default_env --hostname kafka-0 local/kafka:3.2.0
 ```
+
+## Configuration notes
+
+* To generate UUID for the cluster, and make sure the uuid is same for all containers
+
+```bash
+export KAFKA_CLUSTER_UUID=$(./kafka/bin/kafka-storage.sh random-uuid)
+```
+
+* The helper script `main.sh` replaces `_` with `.` and `__` with `_` for all environment variables that start with the
+  prefix `KAFKA_CONFIG_` after deleting the prefix, p.s this replacement doesn't affect the values.  
--- main.sh
+++ main.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
+set -e
 
 SERVER_CONFIG_FILE=/kafka/config.properties
 PREFIX=KAFKA_CONFIG_
-set +x
-set -e
 
+#--------------------------------------------------------------------------------------
+# Setting Kafka node.id
+#--------------------------------------------------------------------------------------
 host_name=$(hostname)
 # e.g kafka-12 becomes: 12
-nodeId=${host_name##*-}
+default_node_id=${host_name##*-}
 
-if [ -z "${nodeId}" ];
+if [ -z "${default_node_id}" ] || [ -z "${default_node_id##*[!0-9]*}" ];
 then
-  export KAFKA_CONFIG_node_id="0"
-else
-  export KAFKA_CONFIG_node_id=$nodeId
+  default_node_id=0
 fi
 
-echo "Node id: ${KAFKA_CONFIG_node_id}"
+export KAFKA_CONFIG_node_id="${KAFKA_CONFIG_node_id:-$default_node_id}"
 
-echo "######## Kafka Configuration #########" > $SERVER_CONFIG_FILE
+#--------------------------------------------------------------------------------------
+# Creating configuration file
+#--------------------------------------------------------------------------------------
+echo "#-----------------------------------" > $SERVER_CONFIG_FILE
+echo "# Generated Kafka Configuration     " >> $SERVER_CONFIG_FILE
+echo "#-----------------------------------" >> $SERVER_CONFIG_FILE
 env | grep "^${PREFIX}" | \
  awk -F $PREFIX '{print $2}' | \
  sed -e ':b; s/^\([^=]*\)*__/\1\##/; tb;' | \
  sed -e ':b; s/^\([^=]*\)*_/\1\./; tb;' | \
  sed -e ':b; s/^\([^=]*\)*##/\1\_/; tb;' >> $SERVER_CONFIG_FILE
 
-echo "---------------------------------"
-echo "Kafka Configuration at ${SERVER_CONFIG_FILE}"
-echo "---------------------------------"
 cat $SERVER_CONFIG_FILE
 echo "---------------------------------"
 
-#export KAFKA_CLUSTER_UUID=$(./kafka/bin/kafka-storage.sh random-uuid)
-
+#--------------------------------------------------------------------------------------
+# Setting kafka cluster UUID
+#--------------------------------------------------------------------------------------
 if [ -z "${KAFKA_CLUSTER_UUID}" ];
 then
-  echo "KAFKA_CLUSTER_UUID was not set using arbitrary uuid: A0acq9cPQwymi60FufxF7g"
+  echo "KAFKA_CLUSTER_UUID was not set, using arbitrary default uuid: A0acq9cPQwymi60FufxF7g"
   export KAFKA_CLUSTER_UUID=A0acq9cPQwymi60FufxF7g
 fi
+echo "---------------------------------"
 
-./kafka/bin/kafka-storage.sh format -t ${KAFKA_CLUSTER_UUID} -c ${SERVER_CONFIG_FILE}
-./kafka/bin/kafka-server-start.sh ${SERVER_CONFIG_FILE}
+#--------------------------------------------------------------------------------------
+# Running kafka
+#--------------------------------------------------------------------------------------
+#./kafka/bin/kafka-storage.sh format -t ${KAFKA_CLUSTER_UUID} -c ${SERVER_CONFIG_FILE}
+#./kafka/bin/kafka-server-start.sh ${SERVER_CONFIG_FILE}
